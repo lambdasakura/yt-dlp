8b76989ea3e41d8a144dfed243848c028f630b27
lambdasakura <lambda.sakura@gmail.com>
2023-05-19 23:39:43 +0900

    Add support for paid user

--- yt_dlp/extractor/bandaichannel.py
+++ yt_dlp/extractor/bandaichannel.py
@@ -25,9 +25,19 @@ class BandaiChannelIE(BrightcoveNewBaseIE):
     def _real_extract(self, url):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
+
+        query = {}
+        for cookie in self.cookiejar:
+          if (cookie.domain == ".b-ch.com" and cookie.name == "BCHWWW"):
+              query = {'mbssn_key': cookie.value.strip()}
+              break
+          
+        if not 'mbssn_key' in query:
+          self.report_warning("If you want to download paid videos, please specify the option `--cookies` or `--cookies-from-browser` when executing the download.")
+
         attrs = extract_attributes(self._search_regex(
             r'(<video-js[^>]+\bid="bcplayer"[^>]*>)', webpage, 'player'))
         bc = self._download_json(
             'https://pbifcd.b-ch.com/v1/playbackinfo/ST/70/' + attrs['data-info'],
-            video_id, headers={'X-API-KEY': attrs['data-auth'].strip()})['bc']
+            video_id, headers={'X-API-KEY': attrs['data-auth'].strip()}, query=query)['bc']
         return self._parse_brightcove_metadata(bc, bc['id'])
